d34b04894b0ef01c4d67ca63ea1bce41b407ade9
MynotesoracleDBA <raghuldrag@gmail.com>
2023-06-13 16:59:03 +0530

    Monitoring EC2 Resources
    Using Flask and boto3 we can monitor the AWS EC2 resources.

--- Using flask api and boto3/Flask-api-development/flask-app.py
+++ Using flask api and boto3/Flask-api-development/flask-app.py
@@ -3,6 +3,8 @@ import boto3
 
 app = Flask(__name__)
 ec2 = boto3.resource('ec2')
+sns = boto3.client('sns')
+
 
 @app.route('/ec2/instances', methods=['GET'])
 def get_all_instances():
@@ -24,14 +26,14 @@ def get_all_instances():
         instance_list.append(instance_dict)
     return jsonify(instance_list)
 
-@app.route('/ec2/instances/start', methods=['POST'])
+@app.route('/ec2/instances/start', methods=['GET'])
 def start_all_instances():
     instances = ec2.instances.all()
     instance_ids = [instance.id for instance in instances]
     ec2.instances.filter(InstanceIds=instance_ids).start()
     return jsonify({'message': 'EC2 instances are starting...'})
 
-@app.route('/ec2/instances/stop', methods=['POST'])
+@app.route('/ec2/instances/stop', methods=['GET'])
 def stop_all_instances():
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     instance_ids = [instance.id for instance in instances]
@@ -39,6 +41,18 @@ def stop_all_instances():
     return jsonify({'message': 'Running EC2 instances are stopping...'})
 
 
+@app.route('/ec2/instances/<instance_id>', methods=['GET'])
+def delete_instance(instance_id):
+    instance = ec2.Instance(instance_id)
+    response = instance.terminate()
+    return jsonify({'message': f'EC2 instance {instance_id} is being terminated.'})
+
+
+def send_sns_notification(instance_id):
+    topic_arn = 'arn:aws:sns:ap-south-1:36676095:Deleted-resource-notification'
+    message = f'EC2 instance {instance_id} has been terminated.'
+    sns.publish(TopicArn=topic_arn, Message=message)
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("8080"), debug=True)
+    app.run( port=int("8080"), debug=True)
 
